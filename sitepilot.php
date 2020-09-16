297c4ea01abb89a106ecefc5ba703929ef92af49
Nick Jansen <n.jansen@sitepilot.io>
2020-09-16 16:57:12 +0200

    Update version

--- sitepilot.php
+++ sitepilot.php
@@ -4,7 +4,7 @@
  * Plugin Name: Sitepilot
  * Plugin URI: https://sitepilot.io
  * Description: A plugin for managing and developing WordPress websites.
- * Version: 0.0.0
+ * Version: 2.0.0-{dev}
  * Author: Sitepilot
  * Author URI: https://sitepilot.io
  * Copyright: (c) 2020 Sitepilot
