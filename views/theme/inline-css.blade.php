ab716f9192f71549de9bf12fc514f6ce5758a6de
Nick Jansen <n.jansen@sitepilot.io>
2021-03-16 16:19:56 +0100

    Update inline-css.blade.php

--- views/theme/inline-css.blade.php
+++ views/theme/inline-css.blade.php
@@ -8,7 +8,8 @@
 }
 
 @foreach($theme->colors() as $color)
-.has-{{ $color->key }}-color {
+.has-{{ $color->key }}-color,
+.has-{{ $color->key }}-text-color {
     color: var(--sp-color-{{ $color->key }});
 }
 
@@ -16,3 +17,9 @@
     background-color: var(--sp-color-{{ $color->key }});
 }
 @endforeach
+
+@if(apply_filters('sp_hide_recaptcha_badge', false))
+.grecaptcha-badge {
+display: none !important;
+}
+@endif
